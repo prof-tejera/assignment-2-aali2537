ed024b62a494ce8faec982d96a5bd83902408512
aali2537 <alexander_li1@student.uml.edu>
2021-11-14 03:39:40 -0500

    Deleted outdated documentation

--- src/views/DocumentationView.js
+++ src/views/DocumentationView.js
@@ -6,8 +6,6 @@ import DocumentComponent from "../components/documentation/DocumentComponent";
 import Loading from "../components/generic/Loading";
 import Button from "../components/generic/Button";
 import Circle from "../components/generic/Circle";
-import DisplayRound from "../components/generic/DisplayRound";
-import DisplayTimer from "../components/generic/DisplayTimer";
 import Panel from "../components/generic/Panel";
 
 const Container = styled.div`
@@ -87,55 +85,6 @@ class Documentation extends React.Component {
                 description:
                   "The rest of the props are either passed along to other props (explained in documentation of those props) for use or are made for plans of future functionality but currently are not used.",
                 type: "N/A",
-                defaultValue: "N/A",
-              },
-            ]}
-          />
-          <DocumentComponent
-            title="DisplayTimer "
-            component={
-              <DisplayTimer minutes={6} seconds={23} milliseconds={67} />
-            }
-            propDocs={[
-              {
-                prop: "minutes",
-                description:
-                  "Current value of timer in minutes. Will display in the leftmost section",
-                type: "string",
-                defaultValue: "0",
-              },
-              {
-                prop: "seconds",
-                description:
-                  "Current value of timer in seconds. Will display in the middle section",
-                type: "string",
-                defaultValue: "0",
-              },
-              {
-                prop: "milliseconds",
-                description:
-                  "Current value of timer in seconds. Will display in the rightmost section",
-                type: "string",
-                defaultValue: "0",
-              },
-            ]}
-          />
-          <DocumentComponent
-            title="DisplayRound "
-            component={<DisplayRound round={5} currentMode="Rest" />}
-            propDocs={[
-              {
-                prop: "round",
-                description:
-                  "Current value of round will display. Will also detect if current timer using component has no rounds and will not display text in such a case. (Component is an svg text element and is not displaying correctly here, please see timers for actual visual)",
-                type: "int",
-                defaultValue: "1",
-              },
-              {
-                prop: "currentMode",
-                description:
-                  "Current mode will display, either Work or Rest mode if inside of a tabata timer. If not inside timer mode text will not display.",
-                type: "string",
                 defaultValue: "N/A",
               },
             ]}
