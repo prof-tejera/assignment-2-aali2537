7560994d7e4dfba47dea34ffc7483fc7b71b0ea7
aali2537 <alexander_li1@student.uml.edu>
2021-11-17 23:38:38 -0500

    Flip Animation

--- src/components/timers/Timer.js
+++ src/components/timers/Timer.js
@@ -6,6 +6,7 @@ import Circle from "../generic/Circle";
 import TextDisplay from "../generic/TextDisplay";
 import Button from "../generic/Button";
 import { TimerContext } from "../context/TimerContext";
+import Settings from "../generic/Settings";
 
 //Circle size settings
 const circleSize = 450;
@@ -26,22 +27,37 @@ const BottomButtonRow = styled.div`
   position: relative;
 `;
 
+const FlipContainer = styled.div`
+  perspective: 1000px;
+`;
+
+const Flipper = styled(Panel)`
+  &:hover {
+    transform: rotateY(180deg);
+  }
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  position: relative;
+`;
+
 const Timer = () => {
   const { btn1, setBtn1 } = useContext(TimerContext);
 
   return (
-    <Panel>
-      <TopButtonRow>
-        <Button icon={"setting"} left={90} />
-      </TopButtonRow>
-      <RelDiv>
-        <Circle size={circleSize} strokeWidth={circleStroke} />
-        <TextDisplay />
-      </RelDiv>
-      <BottomButtonRow>
-        <Button icon={"play"} left={50} />
-      </BottomButtonRow>
-    </Panel>
+    <FlipContainer>
+      <Flipper>
+        <TopButtonRow>
+          <Button icon={"setting"} left={90} />
+        </TopButtonRow>
+        <RelDiv>
+          <Circle size={circleSize} strokeWidth={circleStroke} />
+          <TextDisplay />
+        </RelDiv>
+        <BottomButtonRow>
+          <Button icon={"play"} left={50} />
+        </BottomButtonRow>
+      </Flipper>
+    </FlipContainer>
   );
 };
 
