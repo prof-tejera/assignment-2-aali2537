34d3a6bd51eb83391515ca91e69ba1dcb4e0ecff
aali2537 <alexander_li1@student.uml.edu>
2021-11-19 16:34:59 -0500

    Trying to fix deploy

--- src/components/context/TimerContext.js
+++ src/components/context/TimerContext.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import { tabMap } from "../../utils/helpers";
 import { calcRoundTime } from "../../utils/helpers";
 
